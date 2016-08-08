83d29cb6a3eb454a39b24cab71ddd382d084d0bf
mraveri <mraveri@sissa.it>
2016-08-08 13:52:25 +0200

    Started adding the global structure of EFTCAMB

---
+++ eftcamb/9_EFTCAMB_RGR.f90
@@ -0,0 +1,36 @@
+!----------------------------------------------------------------------------------------
+!
+! This file is part of EFTCAMB.
+!
+! Copyright (C) 2013-2016 by the EFTCAMB authors
+!
+! The EFTCAMB code is free software;
+! You can use it, redistribute it, and/or modify it under the terms
+! of the GNU General Public License as published by the Free Software Foundation;
+! either version 3 of the License, or (at your option) any later version.
+! The full text of the license can be found in the file eftcamb/LICENSE at
+! the top level of the EFTCAMB distribution.
+!
+!----------------------------------------------------------------------------------------
+
+!> @file 9_EFTCAMB_RGR.f90
+!! This file contains the RGR algorithm. This operates on general EFT models.
+
+
+!----------------------------------------------------------------------------------------
+!> This module contains the RGR algorithm. This operates on general EFT models.
+
+!> @author Bin Hu, Marco Raveri
+
+module EFTCAMB_ReturnToGR
+
+    use precision
+    use EFTCAMB_abstract_model
+
+    implicit none
+
+contains
+
+end module EFTCAMB_ReturnToGR
+
+!----------------------------------------------------------------------------------------
---
+++ eftcamb/9_EFTCAMB_main.f90
@@ -0,0 +1,37 @@
+!----------------------------------------------------------------------------------------
+!
+! This file is part of EFTCAMB.
+!
+! Copyright (C) 2013-2016 by the EFTCAMB authors
+!
+! The EFTCAMB code is free software;
+! You can use it, redistribute it, and/or modify it under the terms
+! of the GNU General Public License as published by the Free Software Foundation;
+! either version 3 of the License, or (at your option) any later version.
+! The full text of the license can be found in the file eftcamb/LICENSE at
+! the top level of the EFTCAMB distribution.
+!
+!----------------------------------------------------------------------------------------
+
+!> @file 9_EFTCAMB_main.f90
+!! This file contains the general EFTCAMB driver. We have a type that encapsulate all
+!! EFTCAMB parameters and stuff and this is owned by CAMB to do all EFT calculations.
+
+
+!----------------------------------------------------------------------------------------
+!> This module contains the general EFTCAMB driver. We have a type that encapsulate all
+!! EFTCAMB parameters and stuff and this is owned by CAMB to do all EFT calculations.
+
+!> @author Bin Hu, Marco Raveri
+
+module EFTCAMB_main
+
+    use precision
+
+    implicit none
+
+contains
+
+end module EFTCAMB_main
+
+!----------------------------------------------------------------------------------------
